95fef19866152f683e3506cca448790ec9e27bd3
altinkuko <altinkuko@gmail.com>
2022-11-24 18:03:50 +0100

    Added error message on response

--- src/main/java/com/roi/rentalcar/database/entities/Branch.java
+++ src/main/java/com/roi/rentalcar/database/entities/Branch.java
@@ -1,7 +1,6 @@
 package com.roi.rentalcar.database.entities;
 
 import lombok.Data;
-import lombok.Getter;
 
 import javax.persistence.*;
 import java.util.List;
--- src/main/java/com/roi/rentalcar/services/impl/BranchServiceImpl.java
+++ src/main/java/com/roi/rentalcar/services/impl/BranchServiceImpl.java
@@ -9,7 +9,6 @@ import com.roi.rentalcar.services.BranchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
